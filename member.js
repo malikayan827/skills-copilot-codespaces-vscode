a9a7267dffe5d801f2b5795bbe5aaad5a406a2bc
Muhammad Ayan <99364582+malikayan827@users.noreply.github.com>
2023-09-30 12:03:29 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,15 @@
-function skillsMember()
-{
-    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
-    var member = {
-        name: "John",
-        age: 25,
-        skills: skills
-    };
-    console.log(member.skills[2]);
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
 }
 
 
